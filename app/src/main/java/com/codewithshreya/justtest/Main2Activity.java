2d032b74cafc2acde9a85ecc3b79ed4f284330b2
SHREYANSH <shreyanshjain2674@gmail.com>
2020-05-22 21:47:47 +0530

    commit 1

--- app/src/main/java/com/codewithshreya/justtest/Main2Activity.java
+++ app/src/main/java/com/codewithshreya/justtest/Main2Activity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private EditText Email;
+    private EditText Email,pass;
     private Button go;
     private FirebaseAuth firebaseAuth;
 
@@ -29,6 +29,7 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Email = findViewById(R.id.editText);
+        pass = findViewById(R.id.editText616);
         go = findViewById(R.id.button);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -40,14 +41,15 @@ public class Main2Activity extends AppCompatActivity {
                     Email.setError("Required Field");
                 }if (Email.getText().toString().trim().length() < 5){
                     Email.setError("Enter Valid Roll No");
+                }if (pass.getText().toString().trim().length() < 6){
+                    pass.setError("Password Length must be 6");
                 }else {
                     final String mail = Email.getText().toString().trim() + "@nith.ac.in";
-                    firebaseAuth.createUserWithEmailAndPassword(mail,"123456").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    firebaseAuth.createUserWithEmailAndPassword(mail,pass.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()){
-                                Toast.makeText(Main2Activity.this,"Succesfully Created ... Your Email is " + mail +"Password is 123456" ,
-                                        Toast.LENGTH_LONG).show();
+                                Toast.makeText(Main2Activity.this,"Succesfully Created ... Your Email is " + mail , Toast.LENGTH_LONG).show();
                                 firebaseAuth.signOut();
                                 finish();
                                 startActivity(new Intent(Main2Activity.this,MainActivity.class));
